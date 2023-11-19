b0fa1ec9dbc051f25ff813d0a370a6a76dec53b5
Folson Fairley <folsonfairley20@gmail.com>
2023-11-19 14:25:26 -0500

    pizza place week 2 homework completed

---
+++ pizza_place.js
@@ -0,0 +1,6 @@
+let pizza = "Nebula Pizza";
+let NumberOfTopping = 5;
+let message = `welcome to ${pizza}, we offer up to ${NumberOfTopping} toppings on our pizza`;
+if (NumberOfTopping < 10) {
+  console.log("Quality, not quantity.");
+}
