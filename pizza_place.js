aea90feedb37a6ac35e3e882f2b53b1a92c65b51
Folson Fairley <folsonfairley20@gmail.com>
2023-11-19 18:59:30 -0600

    pizza place and read me files were corrected as instructed

--- pizza_place.js
+++ pizza_place.js
@@ -3,4 +3,11 @@ let NumberOfTopping = 5;
 let message = `welcome to ${pizza}, we offer up to ${NumberOfTopping} toppings on our pizza`;
 if (NumberOfTopping < 10) {
   console.log("Quality, not quantity.");
+} else {
+  console.log("more is always better");
 }
+console.log(message);
+console.log(pizza);
+console.log(typeof pizza);
+console.log(NumberOfTopping);
+console.log(typeof NumberOfTopping);
